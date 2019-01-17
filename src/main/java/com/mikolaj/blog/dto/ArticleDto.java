9dac4ed11a03b13eb8e69a4235878967bcac9ab9
Mikołaj Cygert <miki@comsite.com.pl>
2019-01-17 21:58:19 +0100

    feature/dto Added Dto for Articles

---
+++ src/main/java/com/mikolaj/blog/dto/ArticleDto.java
@@ -0,0 +1,53 @@
+package com.mikolaj.blog.dto;
+
+import com.mikolaj.blog.model.Type;
+
+import java.time.LocalDate;
+
+public class ArticleDto {
+    private Long id;
+    private String text;
+    private Type type;
+    private LocalDate created;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
